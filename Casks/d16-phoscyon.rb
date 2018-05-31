2e89ce92149f49cf6cb25ca466f5874aeb1dc58d
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:39:23 -0700

    Add d16-phoscyon 1.9.0

--- Casks/d16-phoscyon.rb
+++ Casks/d16-phoscyon.rb
@@ -1,6 +1,6 @@
 cask 'd16-phoscyon' do
-  version '1.8.0'
-  sha256 '6d50d510924934055062c9772e0346b47684eedc81d7047e78834df86807e874'
+  version '1.9.0'
+  sha256 '2f45f37eaa744e8165472ee729d5e53fb0b2ad10d2aee06646da87e6ecc4a3a4'
 
   url "http://d16.pl/pub/install/Phoscyon%20-%20#{version}.dmg"
   name 'D16 Phoscyon'
