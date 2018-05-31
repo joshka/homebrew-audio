870e2114b033bb8e1ac1d41b9a9cb5c23a5515e3
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 01:06:03 -0700

    Add d16-tekturon 1.0.2

--- Casks/d16-tekturon.rb
+++ Casks/d16-tekturon.rb
@@ -1,6 +1,6 @@
 cask 'd16-tekturon' do
-  version '1.0.1'
-  sha256 '84d1c6fbae1733f55638addc33c803c82046347da8f54c69a2e62e6b4ee63827'
+  version '1.0.2'
+  sha256 '787f9fa110215628a081341b5df2124b288555c8591d61bb33781ec3361964fe'
 
   url "http://d16.pl/pub/install/Tekturon%20-%20#{version}.dmg"
   name 'D16 Tekturon'
