6c4e53f5c423ad49b9731549e1e9c74e1aeb484c
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-30 23:58:29 -0700

    Add d16-fazortan 1.3.1

--- Casks/d16-fazortan.rb
+++ Casks/d16-fazortan.rb
@@ -1,6 +1,6 @@
 cask 'd16-fazortan' do
-  version '1.3.0'
-  sha256 '5414464f87e88965965c0cd18471722b4b0079a6d82b58e09a45dd605714578d'
+  version '1.3.1'
+  sha256 '9c9d0a1a716ec0d4ffe676bc1e15b0cf4e113d7dea07262eca07ca04743dd4e8'
 
   url "http://d16.pl/pub/install/Fazortan%20-%20#{version}.dmg"
   name 'D16 Fazortan'
