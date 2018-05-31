8ee84eecd3fcf8cd662973b0dc8526c40ee2b9b4
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 01:03:06 -0700

    Add d16-syntorus 1.4.0

--- Casks/d16-syntorus.rb
+++ Casks/d16-syntorus.rb
@@ -1,6 +1,6 @@
 cask 'd16-syntorus' do
-  version '1.3.1'
-  sha256 '8274c84d9574e3acd50773f1d96d00dd1f584620746e15a4476f7b2b1fadaa5a'
+  version '1.4.0'
+  sha256 'c4ac1522630cef6ec0298aae10958f6f7ab3028e95c5ddbea44ba9f248738ed9'
 
   url "http://d16.pl/pub/install/Syntorus%20-%20#{version}.dmg"
   name 'D16 Syntorus'
