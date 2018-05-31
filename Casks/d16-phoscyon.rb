e1357077206df2c0705ec683937711231c594051
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:38:45 -0700

    Add d16-phoscyon 1.8.0

--- Casks/d16-phoscyon.rb
+++ Casks/d16-phoscyon.rb
@@ -1,12 +1,12 @@
 cask 'd16-phoscyon' do
-  version '1.6.0'
-  sha256 '97b55bd946d219deb5133ab71e393a2407829c382534abbee1511e047adbcd8a'
+  version '1.8.0'
+  sha256 '6d50d510924934055062c9772e0346b47684eedc81d7047e78834df86807e874'
 
   url "http://d16.pl/pub/install/Phoscyon%20-%20#{version}.dmg"
   name 'D16 Phoscyon'
   homepage 'http://d16.pl/phoscyon'
 
-  pkg 'Phoscyon.pkg'
+  pkg "Phoscyon-#{version}.pkg"
 
   uninstall pkgutil: 'com.d16group.pkg.phoscyon'
 end
