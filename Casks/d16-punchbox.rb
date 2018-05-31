220c34b5e9329eaead6b7b42b4e52c78e51aefb2
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:46:38 -0700

    Add d16-punchbox 1.0.3

--- Casks/d16-punchbox.rb
+++ Casks/d16-punchbox.rb
@@ -1,12 +1,12 @@
 cask 'd16-punchbox' do
-  version '1.0.2'
-  sha256 'd39ee53696aeafdf8fb73e64d1d476406b66c5f0c419ed22e7af213512fc0fd5'
+  version '1.0.3'
+  sha256 '72c6fb4975c79ffebc18879188d156ed98c707736044a31e775cbce803697412'
 
-  url "http://d16.pl/pub/install/PunchBox%20-%20#{version}.dmg"
-  name 'D16 PunchBox'
+  url "http://d16.pl/pub/install/Punchbox%20-%20#{version}.dmg"
+  name 'D16 Punchbox'
   homepage 'http://d16.pl/punchbox'
 
-  pkg "PunchBox-#{version}.pkg"
+  pkg "Punchbox-#{version}.pkg"
 
   uninstall pkgutil: 'com.d16group.pkg.punchbox'
 end
