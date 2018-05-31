eb7b6e24411b5fc42bf0393a415c55c6a53e30d6
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-30 23:41:31 -0700

    Add d16-decimort 1.3.1

--- Casks/d16-decimort.rb
+++ Casks/d16-decimort.rb
@@ -1,6 +1,6 @@
 cask 'd16-decimort' do
-  version '1.3.0'
-  sha256 '9110bfe1e170589ca9d9005ca5dda04872b451725ae1900b96ff01da1db56d13'
+  version '1.3.1'
+  sha256 'c2ac6663e167288c536cd3512a061a84d941835868c602825c48acedfc1db312'
 
   url "http://d16.pl/pub/install/Decimort%20-%20#{version}.dmg"
   name 'D16 Decimort'
