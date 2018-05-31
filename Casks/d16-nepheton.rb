e395fb1c1cfb3c893e26c8ecb6269a51d431e470
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:23:45 -0700

    Add d16-nepheton 1.0.5

--- Casks/d16-nepheton.rb
+++ Casks/d16-nepheton.rb
@@ -1,12 +1,12 @@
 cask 'd16-nepheton' do
-  version '1.0.2'
-  sha256 '83cb47ade48376c091736b34b001b2fc21987127fe1f6da8923a02044c81844f'
+  version '1.0.5'
+  sha256 '5bffad51786065b8be76f164c0ec5a115dea2f8f17d5f01b9d020fca0e670a86'
 
   url "http://d16.pl/pub/install/Nepheton%20-%20#{version}.dmg"
   name 'D16 Nepheton'
   homepage 'http://d16.pl/nepheton'
 
-  pkg 'Nepheton.pkg'
+  pkg "NephetonVST-#{version}.pkg"
 
   uninstall pkgutil: 'com.d16group.pkg.nepheton'
 end
