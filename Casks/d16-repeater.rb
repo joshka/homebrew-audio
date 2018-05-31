ed1bb93f0fde9fadf2eb5528b6157a525c425b4b
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:54:41 -0700

    Add d16-repeater 1.1.2

--- Casks/d16-repeater.rb
+++ Casks/d16-repeater.rb
@@ -1,6 +1,6 @@
 cask 'd16-repeater' do
-  version '1.1.1'
-  sha256 '1328f9f8a8235bba6f0a8cd90d1e3b9f83cc1325d56ca88c6558c9fd55aed8cc'
+  version '1.1.2'
+  sha256 '6c95ed360b2258fe795d995e60d29c0c238336570bba593d550c509ec999c0d3'
 
   url "http://d16.pl/pub/install/Repeater%20-%20#{version}.dmg"
   name 'D16 Repeater'
