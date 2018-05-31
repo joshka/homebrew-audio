4cc390c4280d1692df435daab13d4b1678a84fb0
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:58:03 -0700

    Add d16-sigmund 1.1.0

--- Casks/d16-sigmund.rb
+++ Casks/d16-sigmund.rb
@@ -1,6 +1,6 @@
 cask 'd16-sigmund' do
-  version '1.0.0'
-  sha256 'be093b135e6e4d247c80417db88504ec3ccae7e8a1c57942f34518cef2bb5402'
+  version '1.1.0'
+  sha256 '70e3768bf1bbd71c0a2819e9839577ae9e097203219317bced69f93d542b0644'
 
   url "http://d16.pl/pub/install/Sigmund%20-%20#{version}.dmg"
   name 'D16 Sigmund'
