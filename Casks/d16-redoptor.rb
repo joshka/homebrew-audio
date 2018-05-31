f0b1011dc64552d8e526a42462caae42228e0e57
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:51:43 -0700

    Add d16-redoptor 1.4.0

--- Casks/d16-redoptor.rb
+++ Casks/d16-redoptor.rb
@@ -1,6 +1,6 @@
 cask 'd16-redoptor' do
-  version '1.3.1'
-  sha256 'dc96919df046b0b7f15b21243dfd74eda126257e65c1855aed9ebcbbd976ed18'
+  version '1.4.0'
+  sha256 '271d4233dcd68418c6d6447495c2c8816dc378f0d50ee62e72e53b74e6d27d15'
 
   url "http://d16.pl/pub/install/Redoptor%20-%20#{version}.dmg"
   name 'D16 Redoptor'
