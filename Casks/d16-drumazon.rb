e2f7cc4132de86148946c22b7cc3b19fcaf4e2d6
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-30 23:53:47 -0700

    Add d16-drumazon 1.4.4

--- Casks/d16-drumazon.rb
+++ Casks/d16-drumazon.rb
@@ -1,6 +1,6 @@
 cask 'd16-drumazon' do
-  version '1.1.8'
-  sha256 '9d965da7a959c58679c299b5fafd357cb062abeeae218341fff126cee74d4c3f'
+  version '1.4.4'
+  sha256 'bcf5c5366eeab3624af7836251c293a171fb94f35db2707e097989fde093fb58'
 
   url "http://d16.pl/pub/install/Drumazon%20-%20#{version}.dmg"
   name 'D16 Drumazon'
