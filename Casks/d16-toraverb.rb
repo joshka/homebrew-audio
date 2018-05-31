79d1cb8b5c975264b15a56de3c1a9ba3e5bf42d2
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 01:10:57 -0700

    Add d16-toraverb 1.2.1

--- Casks/d16-toraverb.rb
+++ Casks/d16-toraverb.rb
@@ -1,6 +1,6 @@
 cask 'd16-toraverb' do
-  version '1.0.1'
-  sha256 '5718bd444dbed14bbe90c06f413f1f57073ff804e73bcc7423c0e5cfaa87ec54'
+  version '1.2.1'
+  sha256 'd35fd82a40cbe0be6eab87662c83e95f64892d0a4615a57c85dbcd5c148a8cab'
 
   url "http://d16.pl/pub/install/Toraverb%20-%20#{version}.dmg"
   name 'D16 Toraverb'
