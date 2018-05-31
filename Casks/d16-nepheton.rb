d25ba6b1f5d8ddde13bab34c3b851ac234829032
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:29:29 -0700

    Add d16-nepheton 1.6.2

--- Casks/d16-nepheton.rb
+++ Casks/d16-nepheton.rb
@@ -1,6 +1,6 @@
 cask 'd16-nepheton' do
-  version '1.6.1'
-  sha256 'cfd8938338a2df87589a7cb9b196c2a30d26ea2eea51fc5759cc8c3e389f4237'
+  version '1.6.2'
+  sha256 'ab9cf6c788536cb45c3cdfae51c4a677641670122428f747c6ec470eb1eb355c'
 
   url "http://d16.pl/pub/install/Nepheton%20-%20#{version}.dmg"
   name 'D16 Nepheton'
