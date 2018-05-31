a119af9ed02fcabb76458df3d3d4061e276bdd23
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-30 23:49:46 -0700

    Add d16-devastor2 2.0.1

--- Casks/d16-devastor2.rb
+++ Casks/d16-devastor2.rb
@@ -1,6 +1,6 @@
 cask 'd16-devastor2' do
-  version '2.0.0'
-  sha256 '1f226d85243e7124408d0f1fd53447cae79c4190f490be3797819a5159bf2631'
+  version '2.0.1'
+  sha256 '0b287cd2ac46edda62db943f9a9035a9cdf50b1a2748e85fdb912e4001671ac7'
 
   url "http://d16.pl/pub/install/Devastor%202%20-%20#{version}.dmg"
   name 'D16 Devastor 2'
