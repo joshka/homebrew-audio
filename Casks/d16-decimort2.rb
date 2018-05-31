cdc03e1b8347d13db3b8fe1dacba4785dd187dc1
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-30 23:46:03 -0700

    Add d16-decimort2 2.1.2

--- Casks/d16-decimort2.rb
+++ Casks/d16-decimort2.rb
@@ -1,6 +1,6 @@
 cask 'd16-decimort2' do
-  version '2.1.1'
-  sha256 '6b2c24099583fc6f3ea245ca42eba584a772300fbbadeb6409a2956bb02b5ecc'
+  version '2.1.2'
+  sha256 '36557e82c8a2a30bcfabacb571f16b3cdf68e2e0866f962f4176a741c7267451'
 
   url "http://d16.pl/pub/install/Decimort%202%20-%20#{version}.dmg"
   name 'D16 Decimort 2'
