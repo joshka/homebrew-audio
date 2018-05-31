468f83b51fe96c6310246046aff2bc448da4e0ef
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:09:49 -0700

    Add d16-lush-101 1.1.0

--- Casks/d16-lush-101.rb
+++ Casks/d16-lush-101.rb
@@ -1,6 +1,6 @@
 cask 'd16-lush-101' do
-  version '1.0.1'
-  sha256 'b44d612290ae8a064845f235f193de49ecd1ed025f09b998726d789beb01304f'
+  version '1.1.0'
+  sha256 '0292bac0c5693c3f43e09c17b121bc7525000188448118caadfdd50012dbc54a'
 
   url "http://d16.pl/pub/install/LuSH-101%20-%20#{version}.dmg"
   name 'D16 LuSH-101'
