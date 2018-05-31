dba88fbb1eaaf46db6ebb0740006b57a8687f6c4
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-30 23:47:58 -0700

    Add d16-devastor 1.3.0

--- Casks/d16-devastor.rb
+++ Casks/d16-devastor.rb
@@ -1,6 +1,6 @@
 cask 'd16-devastor' do
-  version '1.2.0'
-  sha256 'cf04567f1a2a415b998235bff4fe6004515e735549cc5ee846ebfdb0ce430233'
+  version '1.3.0'
+  sha256 'd7919a127d9fab92f476d9d30bea3e835a451e4d634ffd7240a81e3b359cf9c9'
 
   url "http://d16.pl/pub/install/Devastor%20-%20#{version}.dmg"
   name 'D16 Devastor'
