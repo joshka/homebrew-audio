a65b29d21add5a6f022ece471e2cd80274e3354b
Joshua McKinney <joshka@users.noreply.github.com>
2018-05-31 00:32:20 -0700

    Add d16-nithonat 1.6.0

--- Casks/d16-nithonat.rb
+++ Casks/d16-nithonat.rb
@@ -1,6 +1,6 @@
 cask 'd16-nithonat' do
-  version '1.5.1'
-  sha256 'd27ff3e2cb992c20b39cb6b5a5bad82021a7bb184096365e8c5b2215c07bbc53'
+  version '1.6.0'
+  sha256 'ab585351d2e23fcb193973dbe40acd47e13d21ba9c09cc888f16fad28268f129'
 
   url "http://d16.pl/pub/install/Nithonat%20-%20#{version}.dmg"
   name 'D16 Nithonat'
