2d7a9227d7373710184124b8825e0d0590ed9183
Joshua McKinney <joshka@users.noreply.github.com>
2018-07-10 00:03:28 -0700

    Add arturia-dark-ambient 1.1.0.157

---
+++ Casks/arturia-dark-ambient.rb
@@ -0,0 +1,12 @@
+cask 'arturia-dark-ambient' do
+  version '1.1.0.157'
+  sha256 '4cb3fa4aaf89b7dbafa9fe711a2368262a9db90ca6cfe483f8dd7fae09242276'
+
+  url "https://downloads.arturia.net/products/dark_ambient/soft/Dark_Ambient_#{version.dots_to_underscores}.pkg"
+  name 'Arturia Dark Ambient'
+  homepage 'https://www.arturia.com/products/dark_ambient/free'
+
+  pkg "Dark_Ambient_#{version.dots_to_underscores}.pkg"
+
+  uninstall pkgutil: 'com.arturia.pkg.Dark Ambient'
+end
