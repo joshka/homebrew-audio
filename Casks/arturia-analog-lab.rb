4d48f5866f531b43ae57bdf6fc0bfa2b29a5366a
Joshua McKinney <joshka@users.noreply.github.com>
2018-07-09 22:58:06 -0700

    Update arturia-analog-lab to 3.2.0.1509

--- Casks/arturia-analog-lab.rb
+++ Casks/arturia-analog-lab.rb
@@ -1,10 +1,18 @@
 cask 'arturia-analog-lab' do
-  version '1.2.4.126'
-  sha256 'e66182ba1887d0c1369299c9c7ba6a06d709083b8aa6c8163108973d99b7e220'
+  version '3.2.0.1509'
+  sha256 '35e76826c0a10687f75b923fffcdb55272e3d8a5d3383fcf38436a0826bdc2b9'
 
-  url "https://downloads.arturia.com/products/analoglab/soft/Analog_Lab_#{version.dots_to_underscores}.pkg"
+  url "https://updates.arturia.net/products/analoglab/soft/Analog_Lab_3_#{version.dots_to_underscores}.pkg"
   name 'Arturia Analog Lab'
   homepage 'http://www.arturia.com/products/analog-classics/analoglab/overview'
 
-  pkg "Analog_Lab_#{version.dots_to_underscores}.pkg"
+  pkg "Analog_Lab_3_#{version.dots_to_underscores}.pkg"
+
+  uninstall pkgutil: ['com.arturia.pkg.Analog Lab 3_AAX',
+                      'com.arturia.pkg.Analog Lab 3_AudioUnit',
+                      'com.arturia.pkg.Analog Lab 3_Resources',
+                      'com.arturia.pkg.Analog Lab 3_Standalone',
+                      'com.arturia.pkg.Analog Lab 3_ThirdPartyResources',
+                      'com.arturia.pkg.Analog Lab 3_VST',
+                      'com.arturia.pkg.Analog Lab 3_VST3']
 end
