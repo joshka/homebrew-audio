1203d4d2134a061d1b8f8a65d03532d4bc95860a
Joshua McKinney <joshka@users.noreply.github.com>
2018-07-09 22:56:25 -0700

    Update arturia-software-center to 1.4.4.536

--- Casks/arturia-software-center.rb
+++ Casks/arturia-software-center.rb
@@ -1,10 +1,13 @@
 cask 'arturia-software-center' do
-  version '1.1.3.145'
-  sha256 '92e8195ff52cc2870a736b400d192917a16aca92560f94ce0a6f592e5fcc4934'
+  version '1.4.4.536'
+  sha256 '59d039847f7094a283354c71a75bdf002cc16f795201749d0628ca96cb26e127'
 
   url "http://downloads.arturia.com/infrastructure/asc/soft/Arturia_Software_Center_#{version.dots_to_underscores}.pkg"
   name 'Arturia Software Center'
   homepage 'http://www.arturia.com/support/downloads&manuals'
 
   pkg "Arturia_Software_Center_#{version.dots_to_underscores}.pkg"
+
+  uninstall launchctl: 'com.Arturia.ArturiaSoftwareCenterAgent'
+  uninstall pkgutil: 'com.Arturia.pkg.ArturiaSoftwareCenter'
 end
