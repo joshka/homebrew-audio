fb99bfeb3dbf388b6806da47e124f44ca62311e3
Joshua McKinney <joshka@users.noreply.github.com>
2018-07-09 23:01:24 -0700

    Update arturia-midi-control-center to 1.7.3.678

--- Casks/arturia-midi-control-center.rb
+++ Casks/arturia-midi-control-center.rb
@@ -1,10 +1,12 @@
 cask 'arturia-midi-control-center' do
-  version '1.2.2.429'
-  sha256 'fba9e3e4835ece3c4a030c98a03c10051a0d8e3f103405843ee113e7f620892e'
+  version '1.7.3.678'
+  sha256 '1795d2b7527995c3b4023d19107148b89e7418c19e3de6107fcb8313d538810c'
 
   url "https://downloads.arturia.com/extra/mcc/MIDI_Control_Center_#{version.dots_to_underscores}.pkg"
   name 'Arturia Midi Control Center'
   homepage 'http://www.arturia.com/support/downloads&manuals'
 
   pkg "MIDI_Control_Center_#{version.dots_to_underscores}.pkg"
+
+  uninstall pkgutil: 'com.arturia.pkg.MIDI Control Center_Main'
 end
