3a969d0bf365b1e432e5010ecb4697870147bd8b
Joshua McKinney <joshka@users.noreply.github.com>
2018-07-09 23:48:20 -0700

    Add arturia-house-chords 1.0.0.157

---
+++ Casks/arturia-house-chords.rb
@@ -0,0 +1,12 @@
+cask 'arturia-house-chords' do
+  version '1.0.0.157'
+  sha256 '8578fac0ddd6d6d22ab4a3dbc06e66d8e0d9b80ef4b953d0ac4e1a0ec45fe921'
+
+  url "https://downloads.arturia.net/products/house_chords/soft/House_Chords_#{version.dots_to_underscores}.pkg"
+  name 'Arturia House Chords'
+  homepage 'https://www.arturia.com/products/house_chords/free'
+
+  pkg "House_Chords_#{version.dots_to_underscores}.pkg"
+
+  uninstall pkgutil: 'com.arturia.pkg.House Chords'
+end
