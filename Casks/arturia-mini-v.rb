92e46f514063303646090b14737e56a9c905822b
Joshua McKinney <joshka@users.noreply.github.com>
2018-07-09 23:09:34 -0700

    Update arturia-mini-v to 3.3.0.1391

--- Casks/arturia-mini-v.rb
+++ Casks/arturia-mini-v.rb
@@ -1,10 +1,18 @@
 cask 'arturia-mini-v' do
-  version '2.7.0.76'
-  sha256 '872df33f4dd685525660cdb8b7ac65da7c4c40b9b15a2e7df5b772cd67b853fd'
+  version '3.3.0.1391'
+  sha256 'a9c9e124dacfe82ffdb7f89f0f9b0f29c38df6ef488e8fee6ac76435234fa681'
 
-  url "https://downloads.arturia.com/products/mini-v/soft/Mini_V2_#{version.dots_to_underscores}.pkg"
+  url "https://downloads.arturia.com/products/mini-v/soft/Mini_V3_#{version.dots_to_underscores}.pkg"
   name 'Arturia Mini V'
   homepage 'http://www.arturia.com/products/analog-classics/mini-v/overview'
 
-  pkg "Mini_V2_#{version.dots_to_underscores}.pkg"
+  pkg "Mini_V3_#{version.dots_to_underscores}.pkg"
+
+  uninstall pkgutil: ['com.arturia.pkg.Mini V3_ThirdPartyResources',
+                      'com.arturia.pkg.Mini V3_AAX',
+                      'com.arturia.pkg.Mini V3_VST3',
+                      'com.arturia.pkg.Mini V3_VST',
+                      'com.arturia.pkg.Mini V3_AudioUnit',
+                      'com.arturia.pkg.Mini V3_Resources',
+                      'com.arturia.pkg.Mini V3_Standalone']
 end
